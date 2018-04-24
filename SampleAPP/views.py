bc4d4c12149a09c1650532b7fc971ca01f8c434e
devendra5 <35335412+devendra5@users.noreply.github.com>
2018-04-25 00:01:01 +0530

    To Add The SampleApp in GitHub server

--- SampleAPP/views.py
+++ SampleAPP/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def show(request):
-    print(" hello welcome  to hyderabad ")
-    # i am deploy the the project into GitHub...
-
+    pass
 
 
